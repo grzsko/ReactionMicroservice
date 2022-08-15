18f2aef0e6c421fa1ae8f9bae3a560adc7b8dfb2
Grzegorz Skoraczynski <gskoraczynski@gmail.com>
2022-08-15 20:00:22 +0200

    Non core functionalities

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ReactionMicroservice',
+    packages=find_packages(),
+    version='1.0.0',
+    description='A microservice in Python that would run the reaction SMARTS on reactants supplied in SMILES',
+    author='Grzegorz Skoraczynski',
+    license='MIT',
+    python_requires=">=3.9",
+    install_requires=["rdkit"],
+    # entry_points={
+    #     'console_scripts': ['align=Alignstein.command_line:main'],
+    # },
+)
